40e4d3fbbd422d33ea908fcee5636f983b0285a6
DereckAn <dereckangeles15071998@gmail.com>
2023-04-30 18:56:12 -0600

    haciendo la pagina base

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
+
+import { Header } from '../components';
+
 
 const ColorPicker = () => {
   return (
-    <div>ColorPicker</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl '>
+    <Header 
+    title='Color picker' 
+    category="Page"/>
+
+    
+  </div>
   )
 }
 
